76c523d9501b01b97d889ba05a50a99774ac2a8a
viv1rel <1viv1rel@gmail.com>
2025-07-18 03:43:36 +0300

    Выполнено задание блока "Основы" 14

---
+++ www/public/php-basics/14/User.php
@@ -0,0 +1,14 @@
+<?php
+class User
+{
+    public $surname; // фамилия
+    public $name; // имя
+    public $patronymic; // отчество
+
+    public function __construct($surname, $name, $patronymic)
+    {
+        $this->surname = $surname;
+        $this->name = $name;
+        $this->patronymic = $patronymic;
+    }
+}
