d9206ca08a49e1e946b1d5bf1d66b116fef1d06d
viv1rel <1viv1rel@gmail.com>
2025-08-06 01:55:27 +0300

    Выполнено задание блока "Трейты" 46

---
+++ www/public/php-traits/46.php
@@ -0,0 +1,77 @@
+<?php
+trait Helper
+{
+    private $name;
+    private $age;
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+
+class Country
+{
+    use Helper;
+
+    private $population;
+
+    public function __construct($name, $age, $population)
+    {
+        $this->name = $name;
+        $this->age = $age;
+        $this->population = $population;
+    }
+
+    public function getPopulation()
+    {
+        return $this->population;
+    }
+}
+
+$country = new Country('Russia', 1000, 2000);
+echo $country->getName() . "<br>";
+echo $country->getAge() . "<br>";
+echo $country->getPopulation() . "<br>";
+
+trait Trait1
+{
+    private function method1()
+    {
+        return 1;
+    }
+}
+
+trait Trait2
+{
+    private function method2()
+    {
+        return 2;
+    }
+}
+
+trait Trait3
+{
+    private function method3()
+    {
+        return 3;
+    }
+}
+
+class Test
+{
+    use Trait1, Trait2, Trait3;
+
+    public function getSum()
+    {
+        return $this->method1() + $this->method2() + $this->method3();
+    }
+}
+
+$test = new Test();
+echo $test->getSum();
