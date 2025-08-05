867fdc1825c73a5bcb460ad4b541691aee9f10c1
viv1rel <1viv1rel@gmail.com>
2025-08-05 03:00:53 +0300

    Выполнено задание блока "Интерфейсы" 38

---
+++ www/public/php-interfaces/38.php
@@ -0,0 +1,41 @@
+<?php
+interface iUser
+{
+    public function setName($name);
+    public function getName();
+    public function setAge($age);
+    public function getAge();
+}
+
+class User implements iUser
+{
+    private $name;
+    private $age;
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+}
+
+$user = new User();
+$user->setName('john');
+$user->setAge(30);
+
+echo $user->getName() . "<br>";
+echo $user->getAge();
