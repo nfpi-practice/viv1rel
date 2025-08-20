b7e6a87a539ce4368b18907ef5136c0db0ce6579
viv1rel <1viv1rel@gmail.com>
2025-08-20 03:39:48 +0300

    Выполнено задание блока "Использование MVC фреймворка" 93

--- www/public/php-MVC1/project/config/routes.php
+++ www/public/php-MVC1/project/config/routes.php
@@ -3,5 +3,8 @@
 	
 	return [
 		new Route('/hello/', 'hello', 'index'), // роут для приветственной страницы, можно удалить
+        new Route('/act1/', 'test', 'act1'),
+        new Route('/act2/', 'test', 'act2'),
+        new Route('/act3/', 'test', 'act3'),
 	];
 	
