616640608f37aee7f9e26736d0362c3d1318765d
viv1rel <1viv1rel@gmail.com>
2025-08-22 13:44:52 +0300

    Выполнено задание блока "Разработка MVC фреймворка" 105

--- www/public/php-MVC2/index.php
+++ www/public/php-MVC2/index.php
@@ -16,3 +16,4 @@ spl_autoload_register(function($class) {
 $routes = require $_SERVER['DOCUMENT_ROOT'] . '/project/config/routes.php';
 
 $track = ( new Router ) -> getTrack($routes, $_SERVER['REQUEST_URI']);
+$page  = ( new Dispatcher )  -> getPage($track);
