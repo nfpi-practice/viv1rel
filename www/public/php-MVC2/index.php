d002eb3eef51079fef810a47328defd2bff01355
viv1rel <1viv1rel@gmail.com>
2025-08-22 13:49:27 +0300

    Выполнено задание блока "Разработка MVC фреймворка" 106

--- www/public/php-MVC2/index.php
+++ www/public/php-MVC2/index.php
@@ -17,3 +17,5 @@ $routes = require $_SERVER['DOCUMENT_ROOT'] . '/project/config/routes.php';
 
 $track = ( new Router ) -> getTrack($routes, $_SERVER['REQUEST_URI']);
 $page  = ( new Dispatcher )  -> getPage($track);
+
+echo (new View) -> render($page);
